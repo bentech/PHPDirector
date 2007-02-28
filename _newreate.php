166a4470649cffc94c9cedbc832bf8cba05eb63a
Ben Swanson <benswanson1@gmail.com>
2007-02-28 21:04:50 +0000

    made a copy

---
+++ _newreate.php
@@ -0,0 +1,9 @@
+<?php
+
+require('_config-rating.php'); // get the db connection info
+$id_sent = $_GET['q'];
+//getting the values
+$query = "INSERT INTO pp_rating (id, total_votes, total_value, used_ips) VALUES ('$id_sent', '0', '0', '')";
+mysql_query($query);
+
+?>
