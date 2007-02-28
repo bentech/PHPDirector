e0dd06e6a3a1dcdb9776d8ef8847ac2cc36d7b33
Ben Swanson <benswanson1@gmail.com>
2007-02-28 21:05:58 +0000

    made a copy

---
+++ footer.php
@@ -0,0 +1,13 @@
+<div align="center">
+<!--<a href="rss.php">RSS Feed</a> &middot; 
+<a href="#">Most Popular</a> &middot; 
+<a href="#">Today's Favorites</a> &middot; 
+<a href="#">All Time Favorites</a><br>-->
+PHPDirector 2007 is &copy; Copyright of <a href="http://www.crossstar.co.uk/phpdirector/">PHPDirector Group</a>
+</div>
+</body></html>
+<?php
+mysql_close($mysql_link);
+session_destroy();
+?>
+
