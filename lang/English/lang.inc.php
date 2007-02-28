cd7875352eef2fdd3e1c3dd49be2947d25f33abc
Ben Swanson <benswanson1@gmail.com>
2007-02-28 21:02:13 +0000

    made a copy

---
+++ lang/English/lang.inc.php
@@ -0,0 +1,30 @@
+<?php
+/*
++ ----------------------------------------------------------------------------+
+|     PHPDirector - Language File.
+|     $Author: Ben Swanson
++----------------------------------------------------------------------------+
+*/
+
+define("LAN_1", "Error");
+
+
+define("LAN_20", "Good Video but no thanks, this is the example.");
+define("LAN_21", "No Link submitted, would you like to submit a video?");
+define("LAN_22", "This video already exists.");
+define("LAN_23", "Please enter a valid link.");
+define("LAN_24", "Thanks for sumbitting.");
+define("LAN_25", "It will be reviewed shortly.");
+define("LAN_26", "Sorry this video has been rejected.");
+define("LAN_27", "This Video Doesnt Exist");
+define("LAN_28", "Please do not browse videos this way.");
+define("LAN_29", "Nothing to Display");
+
+define("LAN_30", "New way to browse Videos");
+define("LAN_31", "Rating");
+define("LAN_32", "Views");
+define("LAN_33", "Name");
+define("LAN_34", "Date");
+define("LAN_35", "Description");
+define("LAN_36", "Made by");
+?>
---
+++ lang/German/lang.inc.php
@@ -0,0 +1,31 @@
+<?php
+/*
++ ----------------------------------------------------------------------------+
+|     PHPDirector - Language File.
+|     $Author: Michael Merkens
++----------------------------------------------------------------------------+
+*/
+
+define("LAN_1", "Fehler");
+
+
+//process
+define("LAN_20", "Gutes Video, aber nein Danke, dies ist nur ein Beispiel");
+define("LAN_21", "Kein Link uebertragen, moechtest du ein Video hinzufuegen");
+define("LAN_22", "Dieses Video existriert bereits");
+define("LAN_23", "Bitte gib einen gueltigen Link an");
+define("LAN_24", "Danke fuer das Video");
+define("LAN_25", "Es wird in kuerze geprueft");
+define("LAN_26", "Wir bitten um Entschuldigung, das Video wurde abgelehnt");
+define("LAN_27", "Dieses Video existriert nicht");
+define("LAN_28", "Bitte schau dir die Video nicht auf diesem Weg an");
+define("LAN_29", "Nichts zum darstellen");
+
+define("LAN_30", "Neuer Weg zum anzeigen der Videos");
+define("LAN_31", "Bewertung");
+define("LAN_32", "Zuschauer");
+define("LAN_33", "Name");
+define("LAN_34", "Datum");
+define("LAN_35", "Beschreibung");
+define("LAN_36", "Autor");
+?>
