1d394695b372b4b1c1e97fd43dbc3d56ecc62445
Ben Swanson <benswanson1@gmail.com>
2007-02-28 21:09:21 +0000

    made a copy

---
+++ video_functions.php
@@ -0,0 +1,14 @@
+<?php
+class video_views{
+	function add_views(){
+		$new_views = $row["views"] + 1;
+		mysql_query("UPDATE pp_files SET views = '$new_views'
+		WHERE id = '$_GET[$id]'");
+	}
+	function get_views(){
+		$display_video_views = $row["views"];
+		echo LAN_32.":<b>$display_video_views</b>";
+	}
+}
+add_views();
+?>
