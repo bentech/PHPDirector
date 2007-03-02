21df0bf2735b8f2e997183053877b2e35c74abc9
dennis56 <dennis56@phpdirector.co.uk>
2007-03-02 02:38:38 +0000

    patch 1.0

--- admin/index.php
+++ admin/index.php
@@ -10,14 +10,33 @@ if (!isset($_SESSION['phpdirector'])
     header('Location: login.php');
     exit;
 }
-
 ?>
-
+<html>
 <head>
 <body>
 
 <?php include("admin_header.php")?>
 <h2 align="center">Welcome To Admin</h2>
 <p>Please select a tab choosing what to do </p>
+<br>
+<br>
+<?php 
+$result = mysql_query("SELECT * FROM pp_config");
+$row = mysql_fetch_array($result);
+
+echo"
+<b>Your Version :</b> $row[version] 
+<br>
+<script type='text/javascript' src='http://www.crossstar.co.uk/phpdirector_test/version/version.js'>
+</script>
+<b>Your Patch Version:</b> 1
+<br>
+<br>
+<br>
+<br>
+<b>Could it be time to upgrade?</b> <a href'http://www.crossstar.co.uk/phpdirector_test/version/'>Upgrade!</a>
+";
+?>
+
 </body>
 </html>
