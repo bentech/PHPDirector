14ab4b057293263c1f7f53c718688434d4c19874
dennis56 <dennis56@phpdirector.co.uk>
2007-03-04 04:02:22 +0000

    image bug

--- index.php
+++ index.php
@@ -145,33 +145,33 @@ exit;
 <p>
 <b>&nbsp;&nbsp;&nbsp;Sort By:&nbsp;</b>
 <?php echo LAN_31; //Ratings?>
-<a href="?sort=rating&amp;order=up" onmouseout="MM_swapImgRestore()" onmouseover="MM_swapImage('rateup','','images/arrowupani.gif',1)">
+<a href="?sort=rating&order=up" onmouseout="MM_swapImgRestore()" onmouseover="MM_swapImage('rateup','','images/arrowupani.gif',1)">
 <img src="images/arrowup.gif" name="rateup" border="0" id="rateup" title="Worst Rated Videos" alt="arrow up" /></a>
 
-<a href="?sort=rating&amp;order=down" onmouseout="MM_swapImgRestore()" onmouseover="MM_swapImage('ratedwn','','images/arrowdownani.gif',1)"><img src="images/arrowdown.gif" name="ratedwn" border="0" id="ratedwn" title="Best Rated Videos" alt="arrow down" /></a>
+<a href="?sort=rating&order=down" onmouseout="MM_swapImgRestore()" onmouseover="MM_swapImage('ratedwn','','images/arrowdownani.gif',1)"><img src="images/arrowdown.gif" name="ratedwn" border="0" id="ratedwn" title="Best Rated Videos" alt="arrow down" /></a>
 
 &nbsp;
 <?php echo LAN_32; //views ?>
 
-<a href="?sort=views&amp;order=up" onmouseout="MM_swapImgRestore()" onmouseover="MM_swapImage('viewup','','images/arrowupani.gif',1)">
+<a href="?sort=views&order=up" onmouseout="MM_swapImgRestore()" onmouseover="MM_swapImage('viewup','','images/arrowupani.gif',1)">
 <img src="images/arrowup.gif" name="viewup" border="0" id="viewup" title="Least Viewed Videos" alt="arrow up" /></a>
 
-<a href="?sort=views&amp;order=down" onmouseout="MM_swapImgRestore()" onmouseover="MM_swapImage('viewdwn','','images/arrowdownani.gif',1)"><img src="images/arrowdown.gif" name="viewdwn" border="0" id="viewdwn" title="Most Viewed Videos" alt="arrow down" /></a>
+<a href="?sort=views&order=down" onmouseout="MM_swapImgRestore()" onmouseover="MM_swapImage('viewdwn','','images/arrowdownani.gif',1)"><img src="images/arrowdown.gif" name="viewdwn" border="0" id="viewdwn" title="Most Viewed Videos" alt="arrow down" /></a>
 
 &nbsp;
 <?php echo LAN_33; //name ?>
 
-<a href="?sort=name&amp;order=up" onmouseout="MM_swapImgRestore()" onmouseover="MM_swapImage('nameup','','images/arrowupani.gif',1)">
+<a href="?sort=name&order=up" onmouseout="MM_swapImgRestore()" onmouseover="MM_swapImage('nameup','','images/arrowupani.gif',1)">
 <img src="images/arrowup.gif" name="nameup" border="0" id="nameup" title="A-Z" alt="arrow up" /></a>
 
-<a href="?sort=name&amp;order=down" onmouseout="MM_swapImgRestore()" onmouseover="MM_swapImage('namedwn','','images/arrowdownani.gif',1)"><img src="images/arrowdown.gif" name="namedwn" border="0" id="namedwn" title="Z-A" alt="arrow down" /></a>
+<a href="?sort=name&order=down" onmouseout="MM_swapImgRestore()" onmouseover="MM_swapImage('namedwn','','images/arrowdownani.gif',1)"><img src="images/arrowdown.gif" name="namedwn" border="0" id="namedwn" title="Z-A" alt="arrow down" /></a>
 
 &nbsp;
 <?php echo LAN_34; //date ?>
 
-<a href="?sort=date&amp;order=up" onmouseout="MM_swapImgRestore()" onmouseover="MM_swapImage('dateup','','images/arrowupani.gif',1)"> <img src="images/arrowup.gif" name="dateup" border="0" id="dateup" title="Least Recent Videos" alt="arrow up" /></a>
+<a href="?sort=date&order=up" onmouseout="MM_swapImgRestore()" onmouseover="MM_swapImage('dateup','','images/arrowupani.gif',1)"> <img src="images/arrowup.gif" name="dateup" border="0" id="dateup" title="Least Recent Videos" alt="arrow up" /></a>
 
-<a href="?sort=date&amp;order=down" onmouseout="MM_swapImgRestore()" onmouseover="MM_swapImage('datedown','','images/arrowdownani.gif',1)"><img src="images/arrowdown.gif" name="datedown" border="0" id="datedown" title="Most Recent Videos" alt="arrow down" /></a>
+<a href="?sort=date&order=down" onmouseout="MM_swapImgRestore()" onmouseover="MM_swapImage('datedown','','images/arrowdownani.gif',1)"><img src="images/arrowdown.gif" name="datedown" border="0" id="datedown" title="Most Recent Videos" alt="arrow down" /></a>
 </p>
 </div>
 <?
