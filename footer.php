f3e58e6667fcb747f5c641481cc2cbf63df36aca
dennis56 <dennis56@phpdirector.co.uk>
2007-03-04 08:57:22 +0000

    valid

--- footer.php
+++ footer.php
@@ -1,33 +1,23 @@
-<div align="center">
 <!--<a href="rss.php">RSS Feed</a> &middot; 
 <a href="#">Most Popular</a> &middot; 
 <a href="#">Today's Favorites</a> &middot; 
 <a href="#">All Time Favorites</a><br>-->
 <?php
-
-
 $v3 = ' is &'.ghasdnd.'; Copyright';
 $v1 = ellalgae;
 $v4 = ' '.aadsbr.''.asdga.''.bgsd.'">'.ellalgae.'Dire';
 $v2 = ' 200';
-
 // detach messy
 $detach = $v1;
 unset($v1);
 $v1 = $detach;
-
 // detach pretty, but slower
 eval(detach('$v2'));
-
 $v1 .= 'Direc'.hd23.'';
 $v3 .= ' of';
 $v4 .= 'c'.hd23.' '.ehjrty.'</a>';
 $v2 .= '7';
-
-
-
-echo "$v1$v2$v3$v4";
-
+echo "<center>$v1$v2$v3$v4</center>";
 function detach($v) {
    $e = '$detach = ' . $v . ';';
    $e .= 'unset('.$v.');';
@@ -36,9 +26,18 @@ function detach($v) {
 }
 ?>
 </div>
-</body></html>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</body>
+</html>
 <?php
 mysql_close($mysql_link);
 session_destroy();
 ?>
-
