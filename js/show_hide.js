1e1d5d5480c41338d9d5d6e45a064c07081d82f1
dennis56 <dennis56@phpdirector.co.uk>
2007-03-04 08:08:57 +0000

    adding

---
+++ js/show_hide.js
@@ -0,0 +1,8 @@
+<!--
+	function show_hide(tblid, show) {
+		if (tbl = document.getElementById(tblid)) {
+			if (null == show) show = tbl.style.display == 'none';
+			tbl.style.display = (show ? '' : 'none');
+		}
+	}
+//!-->
