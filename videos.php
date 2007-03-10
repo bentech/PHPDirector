ede6bdc64ff2c4c1a77dbc03090b3e8943c908ff
dennis56 <dennis56@phpdirector.co.uk>
2007-03-10 21:48:16 +0000

    random video

--- videos.php
+++ videos.php
@@ -1,38 +1,31 @@
 <?php
 define("PHPdirector", 1);
-
+if(isset($_GET["id"])){
 $id = $_GET["id"];
 $twomonths = 60 * 60 * 24 * 60 + time();
 setcookie("$id", $id, $twomonths);
-
+}
 // includes
 require("header.php");
 
-// start code
-if ($id == null) {
-    echo "<br><center>".LAN_28."<br /></center>"; //dont browse videsos this way
-	exit;
+if(isset($_GET["id"])){
+$result = mysql_query("SELECT * FROM pp_files WHERE id=$id") or die();  
+}else{
+$result = mysql_query("select * from pp_files WHERE approved='1' AND reject='0' order by rand() limit 1") or die();  
 }
-// Get a specific result from the "pp_files" table
-$result = mysql_query("SELECT * FROM pp_files
-WHERE id=$id") or die();  
-$row = mysql_fetch_array( $result );
-// get the first (and hopefully only) entry from the result
-
+$row = mysql_fetch_array($result);
 ?>
 
 <?php
 if ($row['reject'] == "1") {
-echo "<div align='center'>".LAN_26."</div>"; //Rejected
+echo "<div align='center'>".LAN_26."</div>";
 include("footer.php");
 exit;
 }
 ?>
 <?php 
-///error in this line!
-//if($row['approved'] == "0") {
 if($row['feature'] = null) {
-echo "<p><center>".LAN_27."</center></p>"; //Doesnt exist
+echo "<p><center>".LAN_27."</center></p>";
 include("footer.php");
 exit;
 }
