66aaba62f434bc07d8f0a4a63a60794abd38dbef
dennis56 <dennis56@phpdirector.co.uk>
2007-03-10 05:19:24 +0000

    fixed version upgrade

--- admin/index.php
+++ admin/index.php
@@ -20,23 +20,17 @@ if (!isset($_SESSION['phpdirector'])
 <p>Please select a tab choosing what to do </p>
 <br>
 <br>
-<?php 
-$result = mysql_query("SELECT * FROM pp_config");
-$row = mysql_fetch_array($result);
-
-echo"
-<b>Your Version :</b> $row[version] 
-<br>
 <script type='text/javascript' src='http://www.crossstar.co.uk/phpdirector_test/version/version.js'>
 </script>
-<b>Your Patch Version:</b> 1
-<br>
-<br>
-<br>
-<br>
-<b>Could it be time to upgrade?</b> <a href'http://www.crossstar.co.uk/phpdirector_test/version/'>Upgrade!</a>
-";
-?>
+<script type='text/javascript'>
+version = 0.1
+patch = 1
+if(version == nversion && patch == npatch){
+document.write("<b>Time to upgrade!</b> <a href='http://sourceforge.net/projects/phpdirector/'>Upgrade!</a>")
+}else{
+document.write("<b>Your version is recent. Good Job!</b>")
+}
+</script>
 
 </body>
 </html>
