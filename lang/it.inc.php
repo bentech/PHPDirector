5763cb83f4a0d6913d6f3e1dc09f0cdfa9d92d3c
Ben Swanson <benswanson1@gmail.com>
2007-06-23 22:34:16 +0000

    Italian

---
+++ lang/it.inc.php
@@ -0,0 +1,86 @@
+<?php
+/*
++ ----------------------------------------------------------------------------+
+|     PHPDirector - Language File.
+|     $Author: Ben Swanson
+|     $License: GPL General Public License
+|	$Language: Italian
+|	$Translator: Matteo Ferrabone (desmoteo-at-gmail.com)
++----------------------------------------------------------------------------+
+*/
+/*
+Regular Pages
+*/
+/*
+PLEASE NOTE: I translated the messages and warnings to users and admin in a quite informal way. If a different and more formal translation is needed, please send me an e-mail.
+*/
+$smarty->assign('LAN_1', 'Errore');
+$smarty->assign('LAN_2', 'Evidenziati');
+$smarty->assign('LAN_3', 'Tutti');
+$smarty->assign('LAN_4', 'Immagini');
+$smarty->assign('LAN_5', 'Invia');
+$smarty->assign('LAN_6', 'Admin');  /*Normally used also in Italian*/
+$smarty->assign('LAN_7', 'Ordina per');
+$smarty->assign('LAN_8', 'I Video Meno Votati');
+$smarty->assign('LAN_9', 'I Video Più Votati');
+$smarty->assign('LAN_10', 'I Video Meno Visti');
+$smarty->assign('LAN_11', 'I Video Più Visti');
+$smarty->assign('LAN_12', 'Video Meno Recenti');
+$smarty->assign('LAN_13', 'Video Più Recenti');
+$smarty->assign('LAN_14', 'Guarda');
+$smarty->assign('LAN_15', 'Popup');
+$smarty->assign('LAN_16', 'Da');
+$smarty->assign('LAN_17', 'Numero di Visite');
+$smarty->assign('LAN_18', 'Precedente');
+$smarty->assign('LAN_19', 'Successivo');
+$smarty->assign('LAN_20', 'Bel Video ma no, grazie, questo è solo l\'Esempio.');
+$smarty->assign('LAN_21', 'Nessun Link inviato, vuoi segnalare un Video?');
+$smarty->assign('LAN_22', 'Questo Video è già stato segnalato.');
+$smarty->assign('LAN_23', 'Per favore inserisci un link valido.');
+$smarty->assign('LAN_24', 'Grazie per aver segnalato il video.');
+$smarty->assign('LAN_25', 'Sarà valutato al più presto.');
+$smarty->assign('LAN_26', 'Peccato, questo Video è stato rifiutato.');
+$smarty->assign('LAN_27', 'Questo Video non esiste.');
+$smarty->assign('LAN_28', 'Per favore, non scorrere i Video in questo modo.');
+$smarty->assign('LAN_29', 'Niente da visualizzare');
+$smarty->assign('LAN_30', 'Nuovo modo di curiosare tra i Video');
+$smarty->assign('LAN_31', 'Valutazione');
+$smarty->assign('LAN_32', 'Numero di Visite');
+$smarty->assign('LAN_33', 'Nome');
+$smarty->assign('LAN_34', 'Data');
+$smarty->assign('LAN_35', 'Descrizione');
+$smarty->assign('LAN_36', 'Autore');
+$smarty->assign('LAN_37', 'Esempio');
+$smarty->assign('LAN_38', 'Vuoi inviare un altro Video?');
+$smarty->assign('LAN_39', 'Casuale');
+$smarty->assign('LAN_40', 'Categorie');
+$smarty->assign('LAN_41', 'Home');
+$smarty->assign('LAN_42', 'Logout');
+$smarty->assign('LAN_43', 'Elenca Tutti');
+$smarty->assign('LAN_44', 'Approva questo Video');
+$smarty->assign('LAN_45', 'In Evidenza');
+$smarty->assign('LAN_46', 'Rifiuta questo Video');
+$smarty->assign('LAN_47', 'Approvazione Rapida');
+$smarty->assign('LAN_48', 'Opzioni');
+$smarty->assign('LAN_49', 'Nome del Video');
+$smarty->assign('LAN_50', 'Descrizione');
+$smarty->assign('LAN_51', 'Azione');
+$smarty->assign('LAN_52', 'Approva');
+$smarty->assign('LAN_53', 'Evidenzia');
+$smarty->assign('LAN_54', 'Rifiuta');
+$smarty->assign('LAN_55', 'oppure');
+$smarty->assign('LAN_56', 'Rimuovi');
+$smarty->assign('LAN_57', 'Numero di Video per pagina');
+$smarty->assign('LAN_58', 'Lingua');
+$smarty->assign('LAN_59', 'Tutti i campi sono richiesti, nessuno può essere lasciato vuoto');
+$smarty->assign('LAN_60', 'Nessun Video da approvare');
+$smarty->assign('LAN_61', 'Benvenuto in modalità "admin"');
+$smarty->assign('LAN_62', 'Nome Utente');
+$smarty->assign('LAN_63', 'Password');
+$smarty->assign('LAN_64', 'Aggiungi');
+$smarty->assign('LAN_65', 'Modifica');
+$smarty->assign('LAN_66', 'Potrebbe essere necessario cliccare due volte per aggiornare');
+$smarty->assign('LAN_67', 'Novità');
+$smarty->assign('LAN_68', 'Video');
+$smarty->assign('LAN_69', 'Template');
+?>
