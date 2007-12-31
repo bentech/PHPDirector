ccece32e0c602ac1a28cd5b32b7853ac9c7e4b48
James Moey <jamesmoey@gmail.com>
2007-12-31 16:04:52 +0000

    Batch for loading video for testing purpose.

---
+++ batch/loadTestVideo.php
@@ -0,0 +1,28 @@
+<?php
+  define('SF_ROOT_DIR',    realpath(dirname(__FILE__).'/..'));
+  define('SF_APP',         'frontend');
+  define('SF_ENVIRONMENT', 'prod');
+  define('SF_DEBUG',       false);
+ 
+  require_once(SF_ROOT_DIR.DIRECTORY_SEPARATOR.'apps'.DIRECTORY_SEPARATOR.SF_APP.DIRECTORY_SEPARATOR.'config'.DIRECTORY_SEPARATOR.'config.php');
+  $databaseManager = new sfDatabaseManager();
+  $databaseManager->initialize();
+
+  $youtubeList = array("Zdj9vMH4BfQ", "H7Dz0jc1mUs", "VTinQFsoA8I", "WBU3tIhUXXY", "GVI9oPzmWH4", "-vq0lBK0py8", "vYB7CpwxcUw");
+  $dailymotionList = array("-st-vincent-marry-me", "x3vhkj_animation-procreation-3_shortfilms", "x3s9g8_daft-punk-piano_music", "x1x2r2_skydivergirl-jenn-age-24_extreme");
+  $googlevideoList = array("1047064382297597701", "-191580394852217491", "-2149576815319098675", "881321004838285177", "-92587371559679358");
+  
+  foreach ($youtubeList as $id) {
+    $vp = videoProcessorFactory::getVideoProcessor("http://www.youtube.com/watch?v=".$id);
+    $vp->save(TRUE);    
+  }
+  
+  foreach ($dailymotionList as $id) {
+    $vp = videoProcessorFactory::getVideoProcessor("http://www.dailymotion.com/video/".$id);
+    $vp->save(TRUE);    
+  }
+  
+  foreach ($googlevideoList as $id) {
+    $vp = videoProcessorFactory::getVideoProcessor("http://video.google.com.au/videoplay?docid=".$id);
+    $vp->save(TRUE);    
+  }
